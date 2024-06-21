d0887e661785f620250507bc7363e808220a7578
lursmana1 <128970230+lursmana1@users.noreply.github.com>
2024-06-21 14:54:48 +0400

    added Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Button from './Components/Button/Button';
+import React from "react";
+import Button from "./Components/Button/Button";
 
 const Home = () => {
-    return (
-      <>
-      
-      
-      </>
-    );
+  return (
+    <>
+      <h1>hello </h1>
+    </>
+  );
 };
 
 export default Home;
