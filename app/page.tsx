9d09e1df47f915e6ce79b9e6b3fc1cde67cff241
lursmana1 <128970230+lursmana1@users.noreply.github.com>
2024-06-21 14:08:35 +0400

    Created title for React app

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Button from './Components/Button/Button';
+import React from "react";
+import Button from "./Components/Button/Button";
 
 const Home = () => {
-    return (
-      <>
-      
-      
-      </>
-    );
+  return (
+    <>
+      <h1>Welcome to my React App</h1>
+    </>
+  );
 };
 
 export default Home;
