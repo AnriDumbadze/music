f40b90a4ea55850968e66fc408b5fc1a7fb1ddc4
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-03 22:12:54 +0400

    artist/album component
    created artist/album component

---
+++ app/Components/MusicCard/MusicCard.tsx
@@ -0,0 +1,25 @@
+
+import Icon from "../Icon/Icon";
+import styles from "./MusicCard.module.scss"
+
+type Props = {
+    artistSRC:string;
+    artistName:string;
+    songName:string;
+  };
+  const MusicCard = (props: Props) => {
+    return (
+        <div className={styles.musicCard}>
+            <div className={styles.cardContent}>
+                <Icon src={props.artistSRC}/>
+                <div className={styles.songInfo}>
+                    <span className={styles.artistName}>{props.artistName}</span>
+                    <span className={styles.songName}>{props.songName}</span>
+                </div>
+            </div>
+        </div>
+    );
+  };
+  
+  export default MusicCard;
+  
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,19 @@ import AsideMenu from "./Components/Aside/Aside";
 import styles from "./page.module.scss";
 import Navigation from "./Components/AdminNavigation/Nav";
 import Header from "./Components/Header/Header";
+import MusicCard from "./Components/MusicCard/MusicCard";
 
 const Home = () => {
   return (
     <div className={styles.mainContent}>
       <AsideMenu />
-      
-      <Header/>
+      <div className={styles.mainContent__container}>
+        <Header />
+        <div className={styles.mainContent}>
+          <MusicCard artistSRC={"./artist.png"} artistName={"lursman batonu"} songName={"oxx kovzi"} />
+
+        </div>
+      </div>
     </div>
   );
 };
