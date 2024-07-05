307f88e714c212ed9f932810932eda371ae930f3
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-06 01:37:21 +0400

    Created wrapper for music card component
    Created wrapper for music card component

--- app/Components/MusicCardWrapper/MusicCardWrapper.tsx
+++ app/Components/MusicCardWrapper/MusicCardWrapper.tsx
@@ -7,12 +7,19 @@ type Props = {
   const MusicCardWrapper = (props: Props) => {
     return (
         <div className={styles.wrapper}>
-                <MusicCard artistSRC={"./artist.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
-                <MusicCard artistSRC={"./artist.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
-                <MusicCard artistSRC={"./artist.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
-                <MusicCard artistSRC={"./artist.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
-                <MusicCard artistSRC={"./artist.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
-                <MusicCard artistSRC={"./artist.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+                <MusicCard artistSRC={"./artistCard.png"} artistName={"Emnem"} songName={"lamazo biwo"}/>
+
               
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import AsideMenu from "./Components/Aside/Aside";
 import styles from "./page.module.scss";
+import MusicCardWrapper from "./Components/MusicCardWrapper/MusicCardWrapper";
 
 const Home = () => {
   return (
     <div className={styles.mainContent}>
       <AsideMenu />
       <div className={styles.mainContent__container}>
-        <Header />
         <div className={styles.content}>
 
           {/* FOR TESTING */}
 
-
+          <MusicCardWrapper/>
           
           {/* FOR TESTING */}
 
