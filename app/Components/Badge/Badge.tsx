f9c92f2e9aea3647d1ff2532674ca43f2af81f49
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-11 17:43:04 +0400

    Better Badge Component
    Better Badge Component

--- app/Components/Badge/Badge.tsx
+++ app/Components/Badge/Badge.tsx
@@ -1,43 +1,25 @@
 import styles from "./Badge.module.scss";
 
 type Props = {
-  badgeItem1: string;
-  badgeItem2: string;
-  badgeItem3: string;
-  badgeItem4: string;
-  badgeItem5: string;
-  active?: "all" | "album" | "playlists" | "artists" | "downloaded";
+  badgeItems: string[];
+  activeIndex?: number;
 };
 
 const Badge = (props: Props) => {
-  const { badgeItem1, badgeItem2, badgeItem3, badgeItem4, badgeItem5, active } = props;
+  const { badgeItems, activeIndex } = props;
 
-
-  const getBadgeClass = (item: string) => {
-    switch (active) {
-      case "all":
-        return item === badgeItem1 ? styles.active : '';
-      case "album":
-        return item === badgeItem2 ? styles.active : '';
-      case "playlists":
-        return item === badgeItem3 ? styles.active : '';
-      case "artists":
-        return item === badgeItem4 ? styles.active : '';
-      case "downloaded":
-        return item === badgeItem5 ? styles.active : '';
-      default:
-        return '';
-    }
+  const getBadgeClass = (index: number) => {
+    return index === activeIndex ? styles.active : '';
   };
 
   return (
     <div className={styles.badge}>
       <div className={styles.badgeContent}>
-        <span className={getBadgeClass(badgeItem1)}>{badgeItem1}</span>
-        <span className={getBadgeClass(badgeItem2)}>{badgeItem2}</span>
-        <span className={getBadgeClass(badgeItem3)}>{badgeItem3}</span>
-        <span className={getBadgeClass(badgeItem4)}>{badgeItem4}</span>
-        <span className={getBadgeClass(badgeItem5)}>{badgeItem5}</span>
+        {badgeItems.map((item, index) => (
+          <span key={index} className={getBadgeClass(index)}>
+            {item}
+          </span>
+        ))}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,17 @@
 
 import React from "react";
 import styles from "./page.module.scss";
+import Navigation from "./Components/Navigation/Nav";
+import MusicListItem from "./Components/MusicList/MusicListItem";
 
 const Home = () => {
+  const badgeItems = ["All", "Album", "Playlists", "Artists", "Downloaded"];
+  const activeBadge = "album";  
+  const activeIndex = badgeItems.indexOf(activeBadge.charAt(0).toUpperCase() + activeBadge.slice(1));
+
   return (
     <div className={styles.mainContent}>
-      <AsideMenu />
-      <Navigation />
-      <MusicListItem 
-        image={"image.svg"}
-        songName={"Robbery"}
-        artistName={"Juice World"}
-        rank={"1"}
-        onPlay={function (): void {} }
-        button={"playbtn.svg"}
-      />
+
     </div>
   );
 };
