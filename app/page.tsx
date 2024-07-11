60adbd2f6e64037cab3df842f5d2e7adee33199e
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-11 17:00:24 +0400

    Created Badge Component
    Created Badge Component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,16 @@ import styles from "./page.module.scss";
 const Home = () => {
   return (
     <div className={styles.mainContent}>
+      <AsideMenu />
+      <Navigation />
+      <MusicListItem 
+        image={"image.svg"}
+        songName={"Robbery"}
+        artistName={"Juice World"}
+        rank={"1"}
+        onPlay={function (): void {} }
+        button={"playbtn.svg"}
+      />
     </div>
   );
 };
