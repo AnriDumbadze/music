9c41a10cd76310cf1ccc6ab14069eef159b3aa0c
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-12 02:01:02 +0400

    The finished badge component
    The finished badge component

--- app/Components/Badge/Badge.tsx
+++ app/Components/Badge/Badge.tsx
@@ -1,18 +1,22 @@
 import styles from "./Badge.module.scss";
-import React from 'react';
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  id: number;
+  onClick: (value: number) => void;
+  isActive: boolean;
 };
 
 const Badge = (props: Props) => {
   const { children } = props;
 
   return (
-    <div className={styles.badge}>
-      <div className={styles.badgeContent}>
-        {children}
-      </div>
+    <div
+      className={props.isActive ? styles.active : styles.badgeContent}
+      onClick={() => props.onClick(props.id)}
+    >
+      <div className={styles.badgeContent}>{children}</div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@
 import React from "react";
 import AsideMenu from "./Components/Aside/Aside";
 import styles from "./page.module.scss";
-import Badge from "./Components/Badge/Badge";
 import style from "../app/Components/Badge/Badge.module.scss";
+import BadgeComponent from "./Components/Badge/BadgeElements/BadgeElements";
 
 const Home = () => {
+  const [activeBadge, setActiveBadge] = useState(null);
 
   return (
     <div className={styles.mainContent}>
-      <Badge>{badgeElements}</Badge>
+        <BadgeComponent/>
     </div>
   );
 };
+
 export default Home;
