934b0c9dd55b8222ea67cc8e6d2eef4e6014d12a
Demuria <Silent_perfect@mail.ru>
2024-07-11 10:43:54 +0200

    feat:Creatd Music Card Component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import AsideMenu from "./Components/Aside/Aside";
 import styles from "./page.module.scss";
 import Navigation from "./Components/Navigation/Nav";
+import Icon from "./Components/Icon/Icon";
+import MusicCard from "./Components/MusicCard/Musiccard";
 
 const Home = () => {
   return (
     <div className={styles.mainContent}>
       <AsideMenu />
+      <MusicCard 
+      albumCover="./image50.png"
+      author = "dsds"
+      songTitle = "asdsa"
+      />
       <Navigation />
+      
     </div>
   );
 };
