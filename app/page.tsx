06edf01657905a599ee733e675d287db36b6852a
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-12 14:57:18 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import styles from "./page.module.scss";
+import ArtistCard from "./Components/ArtistCard/ArtistCard";
 
 const Home = () => {
   const [activeBadge, setActiveBadge] = useState<number | null>(null);
 
   return (
     <div className={styles.mainContent}>
+
     </div>
   );
 };
