bce236f97f17fa91ad10bc0e7cb2eaf1ce7b06f9
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-12 14:56:24 +0400

    Created Artist Card Component
    Created Artist Card Component

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,12 @@
 
 import React, { useState } from "react";
 import styles from "./page.module.scss";
-import badgeItems from "@/public/Consts/BadgeData";
-import Badge from "./Components/Badge/Badge";
 
 const Home = () => {
   const [activeBadge, setActiveBadge] = useState<number | null>(null);
 
   return (
     <div className={styles.mainContent}>
-      <div className={styles.item}>
-        {badgeItems.map((item, index) => (
-          <Badge
-            isActive={activeBadge === index}
-            id={index}
-            onClick={setActiveBadge}
-          >
-            {item}
-          </Badge>
-        ))}
-      </div>
     </div>
   );
 };
