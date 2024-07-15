249b8833daad83c47b9ba42aae090bf33ce1af3f
SabaMchedlishvili <mchedlishvilisaba7@gmail.com>
2024-07-15 16:58:10 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,8 @@ import PlayerController from "./Components/PlayerController/PlayerController";
 const Home = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
-
+  const [activeBadge, setActiveBadge] = useState<number | null>(null);
+  
   const handlePlayPause = () => {
     setIsPlaying(!isPlaying);
   };
