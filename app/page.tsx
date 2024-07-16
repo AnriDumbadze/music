b6b994b5efda39d1d25ba6f7e5c1d5ae070920eb
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-16 12:58:57 +0400

    Footer mobile
    created Footer mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.scss";
 import ArtistCard from "./Components/ArtistCard/ArtistCard";
-import MusicWrapper from "./Components/MusicWrapper/MusicWrapper";
-import AsideMenu from "./Components/Aside/Aside";
-import Aside from "./Components/Aside/Aside";
 
 const Home = () => {
+  const [activeBadge, setActiveBadge] = useState<number | null>(null);
 
   return (
     
     <div className={styles.mainContent}>
+
     </div>
   );
 };
