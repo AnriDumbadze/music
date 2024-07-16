6a6ff49387d96209cc151c7251aee7643812bf0a
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-16 15:55:24 +0400

    asd
    asd

--- app/page.tsx
+++ app/page.tsx
@@ -16,29 +16,38 @@ const badgeItems: BadgeItem[] = [
 ];
 
 const Home = () => {
-  const [activeBadge, setActiveBadge] = useState<string>(badgeItems[0].name);
-  const activeIndex = badgeItems.findIndex(item => item.name === activeBadge);
-
-  const badgeElements = badgeItems.map((item, index) => (
-    <span
-      key={index}
-      className={index === activeIndex ? styles.active : ""}
-      onClick={() => setActiveBadge(item.name)}
-    >
-      {item.name}
-    </span>
-  ));
+  const [activeBadge, setActiveBadge] = useState<number>(FooterItems[0].id);
+  
+  const activeItem = FooterItems.find((item) => item.id === activeBadge);
 
   return (
     
     <div className={styles.mainContent}>
       <div className={styles.badgeContainer}>
-        <div className={styles.badgeNames}>
-          {badgeElements}
-        </div>
-        <div className={styles.badgePhoto}>
-          <Footer badgeItems={badgeItems} activeIndex={activeIndex} />
-
+        <div className={styles.badgeContent}>
+          {FooterItems.map((item) => (
+            <div
+              className={styles.badgeMap}
+              key={item.id}
+              onClick={() => setActiveBadge(item.id)}
+            >
+              <div className={styles.badgeNames}>
+                <span
+                  className={item.id === activeBadge ? styles.active : ''}
+                >
+                  {item.name}
+                </span>
+              </div>
+              <div className={styles.footer}>
+                <div className={styles.badgePhotos}>
+                  <img
+                    src={item.id === activeBadge ? item.activePhoto : item.photo}
+                    alt={item.name}  
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       
