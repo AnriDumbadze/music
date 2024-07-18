5c540a8d3926e5ddd236a888d814fa3afa3e7a6f
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-07-18 14:25:16 +0400

    corrected some mistakes
    corrected some mistakes

--- app/Components/PlayerController/PlayerController.tsx
+++ app/Components/PlayerController/PlayerController.tsx
@@ -1,6 +1,6 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './PlayerController.module.scss';
-import Image from 'next/image';
-
+import songs from '@/public/Consts/songs';
 
 type PlayerControllerProps = {
     albumTitle: string;
@@ -9,9 +9,6 @@ type PlayerControllerProps = {
     queueArtist: string;
     photo: string;
     image: string;
-    currentTrack: string;
-    currentArtist: string;
-    currentTime: number;
     duration: number;
     isPlaying: boolean;
     onPlayPause: () => void;
@@ -25,21 +22,59 @@ const PlayerController = (props: PlayerControllerProps) => {
     const {
         albumTitle,
         dropdown,
-        queueArtist,
-        queueTrack,
-        photo,
-        image,
-        currentTrack,
-        currentArtist,
-        currentTime,
         duration,
         isPlaying,
         onPlayPause,
-        onSkipForward,
-        onSkipBackward,
         onRepeat,
         onShuffle,
     } = props;
+
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const [currentTrackTime, setCurrentTrackTime] = useState(0);
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [songEnded, setSongEnded] = useState(false);
+
+    useEffect(() => {
+        if (audioRef.current) {
+            if (isPlaying) {
+                audioRef.current.play();
+            } else {
+                audioRef.current.pause();
+            }
+        }
+    }, [isPlaying]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (audioRef.current && isPlaying) {
+                setCurrentTrackTime(audioRef.current.currentTime);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [isPlaying]);
+
+    useEffect(() => {
+        const handleSongEnd = () => {
+            setSongEnded(true);
+        };
+
+        if (audioRef.current) {
+            audioRef.current.addEventListener('ended', handleSongEnd);
+            return () => {
+                audioRef.current?.removeEventListener('ended', handleSongEnd);
+            };
+        }
+    }, [currentSongIndex]);
+
+    useEffect(() => {
+        if (songEnded) {
+            setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+            setCurrentTrackTime(0);
+            setSongEnded(false);
+            onPlayPause(); // Pause the player when moving to the next song
+        }
+    }, [songEnded]);
 
     const formatTime = (seconds: number) => {
         const mins = Math.floor(seconds / 60);
@@ -47,18 +82,36 @@ const PlayerController = (props: PlayerControllerProps) => {
         return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
     };
 
+    const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const time = Number(e.target.value);
+        setCurrentTrackTime(time);
+        if (audioRef.current) {
+            audioRef.current.currentTime = time;
+        }
+    };
+
+    const handleSkipForward = () => {
+        setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+        setCurrentTrackTime(0);
+    };
+
+    const handleSkipBackward = () => {
+        setCurrentSongIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
+        setCurrentTrackTime(0);
+    };
 
     return (
         <div className={styles.playerController}>
+            <audio ref={audioRef} src={songs[currentSongIndex].url} />
             <div className={styles.arrow}>
                 <img src={dropdown} alt="" />
             </div>
             <p className={styles.albumName}>{albumTitle}</p>
             <div className={styles.trackInfo}>
-                <img src={image} alt="Album Art" className={styles.albumArt} />
+                <img src={songs[currentSongIndex].src} alt="Artist Image" className={styles.albumArt} />
                 <div className={styles.trackDetails}>
-                    <h3 className={styles.track}>{currentTrack}</h3>
-                    <p>{currentArtist}</p>
+                    <h3 className={styles.track}>{songs[currentSongIndex].title}</h3>
+                    <p>{songs[currentSongIndex].artist}</p>
                 </div>
                 <div className={styles.icons}>
                     <img src="icons/plus.svg" alt="" />
@@ -67,33 +120,35 @@ const PlayerController = (props: PlayerControllerProps) => {
             </div>
             <div className={styles.progress}>
                 <div className={styles.current}>
-                    <span>{formatTime(currentTime)}</span>
+                    <span>{formatTime(currentTrackTime)}</span>
                 </div>
                 <input
                     type="range"
                     className={styles.playInput}
                     min="0"
                     max={duration}
-                    value={currentTime}
-                    onChange={() => { }}
+                    value={currentTrackTime}
+                    onChange={handleTimeChange}
                 />
                 <div className={styles.duration}>
-                    <span>{formatTime(duration)}</span>
+                    <span>{songs[currentSongIndex].songDuration}</span>
                 </div>
                 <div className={styles.controls}>
                     <button onClick={onRepeat} className={styles.repeat}><img src="icons/repeat.svg" alt="" /> </button>
-                    <button onClick={onSkipBackward}><img src="icons/previous.svg" alt="" /></button>
-                    <button onClick={onPlayPause}>{isPlaying ? <img src="icons/play.svg" alt="" /> : <img src="icons/play2.svg" alt="" />}</button>
-                    <button onClick={onSkipForward}><img src="icons/next.svg" alt="" /></button>
+                    <button onClick={handleSkipBackward}><img src="icons/previous.svg" alt="" /></button>
+                    <button onClick={onPlayPause}>
+                        {isPlaying ? <img src="icons/pause.svg" alt="Pause" /> : <img src="icons/play.svg" alt="Play" />}
+                    </button>
+                    <button onClick={handleSkipForward}><img src="icons/next.svg" alt="" /></button>
                     <button onClick={onShuffle} className={styles.shuffle}><img src="icons/shuffle.svg" alt="" /> </button>
                 </div>
                 <div className={styles.queue}>
                     <p>In Queue</p>
                     <div className={styles.next}>
-                        <img src={photo} alt="" width={48} height={48} />
+                        <img src={songs[currentSongIndex].queueImg} alt="Queue Image" width={48} height={48} />
                         <div className={styles.names}>
-                            <span className={styles.queueTrack}>{queueTrack}</span>
-                            <span className={styles.queueArtist}>{queueArtist}</span>
+                            <span className={styles.queueTrack}>{songs[currentSongIndex].queueSong}</span>
+                            <span className={styles.queueArtist}>{songs[currentSongIndex].queueName}</span>
                         </div>
                         <div className={styles.image}>
                             <img src="icons/plus.svg" alt="" width={15} />
---
+++ public/Consts/songs.ts
@@ -0,0 +1,6 @@
+const songs = [
+    { title: "Song 1", artist: "Artist 1", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", src: "./images/artist.png", songDuration: "4:12", queueImg: "./images/queue.png", queueSong: "lamazo bijo", queueName: "Lursman del rey"  },
+    { title: "Song 2", artist: "Artist 2", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3", src: "./images/artistFirst.png", songDuration: "4:12", queueImg: "./images/queue.png", queueSong: "cxra mtas gadavivli", queueName: "gabriel pogba"  },
+    { title: "Song 3", artist: "Artist 3", url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3", src: "./images/artistSec.png", songDuration: "4:12", queueImg: "./images/queue.png", queueSong: "chanchxalo ox chanchxalo", queueName: "anri"  },
+];
+export default songs;
