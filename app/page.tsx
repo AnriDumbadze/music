a8f24a7fe4e67021ff9188616c4b97708ed09fbd
Demuria <Silent_perfect@mail.ru>
2024-08-01 11:26:16 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,34 @@
 import React, { useState } from "react";
 import styles from "./page.module.scss";
 import Navigation from "./Components/Navigation/Nav";
-import AsideMenu from "./Components/Aside/Aside";
+import Aside from "./Components/Aside/Aside";
+import LibraryItem from "./Components/LibraryItem/LibraryItem";
+import MobileFooter from "./Components/MobileFooter/MobileFooter";
+import TopChart from "./Components/TopChart/TopChart";
+
 
 const Home = () => {
+  const [query, setQuery] = useState<string>('');
+
   return (
     <div className={styles.mainContent}>
-      <AsideMenu />
+      <Aside />
       <Navigation />
+      <LibraryItem 
+        title={"Lursman del rey"} 
+        songQuantity={"1221"} 
+        onClick={function (value: number): void {
+          throw new Error("Function not implemented.");
+        }} 
+        id={0}
+      />
+      <TopChart 
+        image={"images/topImage.png"}
+        artistName="SZA"
+        songName="SOS!"
+        rank="12"
+      />
+      
     </div>
   );
 };
