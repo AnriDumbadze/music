738a080e1e4d5aa3ce6299f77f4c647d5918396d
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-09-11 13:20:13 +0400

    i make burger menu for mobile

--- app/Components/burgermenumobile/page.tsx
+++ app/Components/burgermenumobile/page.tsx
@@ -31,3 +31,35 @@ export default function BurgerMenuMobile(){
         </>
     )
 }
+import { useState } from 'react'
+import styles from './page.module.scss'
+import LightDark from '../LightDark/LightDark'
+
+export default function BurgerMenuMobile() {
+    const [showBurger, setShowBurger] = useState(false)
+    const [showBtn, setShowBtn] = useState(true)
+
+    const openBurger = () => {
+        setShowBurger(true)
+        setShowBtn(false)
+    }
+
+    const closeBurger = () => {
+        setShowBurger(false)
+        setShowBtn(true)
+    }
+
+    return (
+        <>
+           {showBtn&&  <button onClick={openBurger} className={styles.buttonBurger}><div className={styles.burgerimg}></div></button>}
+            {showBurger && (
+                <div className={styles.overlay}>
+                    <div className={styles.burgerMenu}>
+                        <LightDark/>
+                        <div onClick={closeBurger} className={styles.logoutImg}></div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
