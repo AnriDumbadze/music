11dcc799100daff56f3e222e7790438afa6b71ae
BatoniGyro <46069325+BatoniGyro1@users.noreply.github.com>
2024-10-01 16:29:18 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,55 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.scss";
-import Navigation from "./Components/Navigation/Nav";
-import Aside from "./Components/Aside/Aside";
-import LibraryItem from "./Components/LibraryItem/LibraryItem";
-import MobileFooter from "./Components/MobileFooter/MobileFooter";
+import Aside, { getCookie } from "./Components/Aside/Aside";
 import TopChart from "./Components/TopChart/TopChart";
-import Search from "./Components/SearchComponent/Search";
-import Input from "./Components/Input/input";
-import ButtonIcon from "./Components/ButtonIcon/ButtonIcon";
-import ButtonArrows from "./Components/ButtonArrows/ButtonArrows";
-import { ButtonStyle } from "./Components/ButtonStyles";
 import Header from "./Components/Header/Header";
 import MusicWrapper from "./Components/MusicWrapper/MusicWrapper";
 import MusicCard from "./Components/MusicCard/Musiccard";
 import ArtistCard from "./Components/ArtistCard/ArtistCard";
-import BurgerMenuMobile from "./Components/burgermenumobile/burgetmobile";
 
 const Home = () => {
-  const [query, setQuery] = useState<string>('');
+  const [query, setQuery] = useState<string>("");
+  const [themeColor, setThemeColor] = useState(getCookie("theme") || "");
 
+  useEffect(() => {
+    const updateTheme = () => {
+      const newTheme = getCookie("theme");
+      setThemeColor(String(newTheme));
+    };
+
+    updateTheme();
+
+    const themeInterval = setInterval(updateTheme, 0); // Adjust interval as needed
+
+    return () => clearInterval(themeInterval); 
+  }, []);
+  
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
 
+  const artistCards = [
+    <ArtistCard key={"artist-1"} artistImg={"artist"} artistName={"Travis Scott"} artistType={"Artist"} />,  
+  ];
+
+  const popularHits = [
+    <MusicCard key={"music-1"} albumCover={"popHit"} author={"Juice WRLD"} songTitle={"Robbery"} />,
+  ];
+
+  const popularCharts = [
+    <TopChart key={"chart-1"} image={"topChart"} songName={"Good Days"} artistName={"SZA"} rank={"1"} />,
+  ];
+
   return (
     <div className={styles.mainContent}>
       <Aside />
-      <Navigation />
-      <LibraryItem 
-        title={"Lursman del rey"} 
-        songQuantity={"1221"} 
-        onClick={function (value: number): void {
-          throw new Error("Function not implemented.");
-        }} 
-        id={0}
-      />
-      <TopChart 
-        image={"images/topImage.png"}
-        artistName="SZA"
-        songName="SOS!"
-        rank="12"
-        />
-        
-        <Search onChange={setQuery} />
-      
-        <div>
-        <ButtonIcon 
-        title={""}
-        icon={"icons/music.svg"}
-        onClick={function ():void {} } 
-        style={ButtonStyle.Black} 
-        />
-        </div>
-        <ButtonArrows 
-        direction={"left"} 
-        onClick={function (): void {} } 
-        icon="icons/leftarr.svg"
-        />
+      <div className={`${styles.static} ${themeColor === 'dark' ? styles.darkStatic : ''}`}>
+        <Header />
+        <MusicWrapper cards={artistCards} name={"Popular artists"} />
+        <MusicWrapper cards={popularHits} name={"Popular hits of the week"} />
+        <MusicWrapper cards={popularCharts} name={"Popular Charts"} />
+      </div>
     </div>
   );
 };
