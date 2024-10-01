18263fcb06cb758edca8effd4575e533648a2c33
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-10-01 11:53:01 +0400

    fix

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -6,43 +6,37 @@ import styles from "./Header.module.scss";
 interface Props{
   onchange?: (e: React.ChangeEvent<HTMLInputElement>) => void; 
 }
-const Header = (props:Props) => {
-  const [themeColor, setThemeColor] = useState<string | null>(getCookie("theme")); 
+const Header = (props: Props) => {
+  const [themeColor, setThemeColor] = useState<string | null>(getCookie("theme") ?? null);
+
   useEffect(() => {
     const updateTheme = () => {
-      const newTheme = getCookie("theme");
-      setThemeColor(newTheme)
+      const newTheme = getCookie("theme") ?? null;
+      setThemeColor(newTheme);
     };
 
     updateTheme();
 
-    const themeInterval = setInterval(updateTheme, 0); 
+    const themeInterval = setInterval(updateTheme, 0);
 
     return () => clearInterval(themeInterval);
   }, []);
+
   return (
     <div className={`${styles.header} ${themeColor === 'dark' ? styles.darkHeader : ''}`}>
       <div className={styles.headerContent}>
         <div className={styles.arrowContainer}>
-          <Icon  height={"40px"} width={"40px"} name={"Arrow"} isActive={false} onClick={function (): void {
-            throw new Error("Function not implemented.");
-          } } />
-          <Icon  height={"40px"} width={"40px"} name={"rightArr"} isActive={false} onClick={function (): void {
-            throw new Error("Function not implemented.");
-          } } />
+          <Icon height={"40px"} width={"40px"} name={"Arrow"} isActive={false} onClick={() => {}} />
+          <Icon height={"40px"} width={"40px"} name={"rightArr"} isActive={false} onClick={() => {}} />
         </div>
 
         <div className={styles.searchContainer}>
-          <Icon name={"searchIcon"} isActive={false} onClick={function (): void {
-            throw new Error("Function not implemented.");
-          } }  />
+          <Icon name={"searchIcon"} isActive={false} onClick={() => {}} />
           <input onChange={props.onchange} className={styles.noBorder} placeholder="search" />
         </div>
       </div>
     </div>
-
-    
   );
-};
+}
 
 export default Header;
