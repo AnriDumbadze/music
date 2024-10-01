185762fafe8915cff626bfc6f4d63ef621149bb6
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-10-01 12:02:56 +0400

    haa qna?

--- app/Components/MusicWrapper/MusicWrapper.tsx
+++ app/Components/MusicWrapper/MusicWrapper.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import ArtistCard from "../ArtistCard/ArtistCard";
 import { getCookie } from "../Aside/Aside";
 import styles from "./MusicWrapper.module.scss";
 
 type Props = {
-    cards: JSX.Element[]; 
-    name: string;
+  cards: JSX.Element[];
+  name: string;
 };
 
 const MusicWrapper = (props: Props) => {
-  const [themeColor, setThemeColor] = useState<string | null>(getCookie("theme")); // Store theme in state
+  const [themeColor, setThemeColor] = useState<string | null>(getCookie("theme") ?? null); // Handle undefined case
 
   useEffect(() => {
     const updateTheme = () => {
-      const newTheme = getCookie("theme");
+      const newTheme = getCookie("theme") ?? null; // Handle undefined case
       setThemeColor(newTheme);
     };
 
@@ -22,24 +22,23 @@ const MusicWrapper = (props: Props) => {
 
     const themeInterval = setInterval(updateTheme, 0); // Adjust interval as needed
 
-    return () => clearInterval(themeInterval); 
+    return () => clearInterval(themeInterval);
   }, []);
-    return (
-      <div className={`${styles.wrapper} ${themeColor === 'dark' ? styles.darkWrapper : ''}`}>
-        <div className={styles.wrapperContent}>
-          <h1 className={styles.title}>{props.name}</h1>
-          <div className={styles.content}>
-            {props.cards.map((card, index) => (
-              <div key={index} className={styles.card}>
-                {card}
-              </div>
-            ))}
-          </div>
+
+  return (
+    <div className={`${styles.wrapper} ${themeColor === 'dark' ? styles.darkWrapper : ''}`}>
+      <div className={styles.wrapperContent}>
+        <h1 className={styles.title}>{props.name}</h1>
+        <div className={styles.content}>
+          {props.cards.map((card, index) => (
+            <div key={index} className={styles.card}>
+              {card}
+            </div>
+          ))}
         </div>
       </div>
-    );
-  };
-  
-  export default MusicWrapper;
-  
+    </div>
+  );
+};
 
+export default MusicWrapper;
