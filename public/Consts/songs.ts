7f97b343b29685e3a2c2476010521299f4dff053
sabakhuskivadze <130067710+sabakhuskivadze@users.noreply.github.com>
2024-10-04 00:10:23 +0400

    dsad

--- public/Consts/songs.ts
+++ public/Consts/songs.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const userToken = getCookie("userToken");
 
 // Define the songs array outside of the Axios request
-let songs: any[] = [];
+export let songs: any[] = [];
 
 // Fetch music data from the API
 axios
@@ -18,17 +18,21 @@ axios
     console.log(`Fetched music data:`, musicData);
 
     // Populate the songs array based on fetched music data
-    songs = musicData.map((musicItem: any) => ({
-      id: musicItem.id, // Assuming musicItem has an 'id' property
-      title: musicItem.name, // Assuming musicItem has a 'name' property
-      artist: musicItem.artist || "Unknown Artist", // Use a default if artist is not provided
-      url: musicItem.url || "", // Use a default if url is not provided
-      songDuration: musicItem.duration || "0:00", // Default duration
-      queueSong: musicItem.queueSong || "N/A", // Default queue song
-      queueName: musicItem.queueName || "N/A", // Default queue name
-      src: musicItem.src || "./images/default.png", // Default image source
-      queueImg: musicItem.queueImg || "./images/defaultQueue.png", // Default queue image
-    }));
+    [
+      songs = musicData.map((musicItem: any) => ({
+        id: musicItem.id, // Assuming musicItem has an 'id' property
+        title: musicItem.name, // Assuming musicItem has a 'name' property
+        artist: musicItem.artist,
+        url: musicItem.url,
+        songDuration: musicItem.duration ,
+        queueSong: musicItem.queueSong || "N/A", // Default queue song
+        queueName: musicItem.queueName || "N/A", // Default queue name
+        src: musicItem.src || "./images/default.png", // Default image source
+        queueImg: musicItem.queueImg || "./images/defaultQueue.png", // Default queue image
+      }))
+    ]
+
+    Array.isArray(songs)
 
     // Log the songs array
     console.log(`Processed songs array:`, songs);
@@ -38,4 +42,4 @@ axios
   });
 
 // Export the songs array as a named export
-export { songs };
+export default songs 
