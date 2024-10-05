f1c2a394dc08574cf8e84af3b6b68298d9badd1a
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-10-05 12:12:45 +0400

    asd
    asd

--- app/page.tsx
+++ app/page.tsx
@@ -46,10 +46,11 @@ const Home = () => {
   const [showPlayer, setShowPlayer] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentTrackTime, setCurrentTrackTime] = useState(0);
+
   useEffect(() => {
     const updateTheme = () => {
-        const newTheme = getCookie("theme") || "light"; 
-        setThemeColor(newTheme);
+      const newTheme = getCookie("theme") || "light"; 
+      setThemeColor(newTheme);
     };
 
     updateTheme();
@@ -57,7 +58,8 @@ const Home = () => {
     const themeInterval = setInterval(updateTheme, 0);
 
     return () => clearInterval(themeInterval);
-}, []);
+  }, []);
+
   // Fetch artist data
   useEffect(() => {
     const userToken = getCookie("userToken");
@@ -139,7 +141,7 @@ const Home = () => {
               <MusicWrapper
                 cards={artistData.map((artist) => (
                   <ArtistCard
-                    key={artist.id}
+                    key={artist.id} // Ensuring each ArtistCard has a unique key
                     artistImg={artist.image[artist.image.length - 1]?.url || "/Images/artist.png"}
                     artistName={artist.firstName}
                     artistType="Artist"
@@ -176,7 +178,6 @@ const Home = () => {
                     <div className={styles.videoContainer}>
                       <div onClick={handleBack} className={styles.controllerbutton}>
                         <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          {/* SVG Path for Back button */}
                           <path
                             d="M23.25 6.66663C23.25 5.93329 22.6667 5.33329 21.9167 5.33329C21.55 5.33329 21.1833 5.44996 20.9167 5.68329L12.85 12.1491C11.9167 12.8666 11.9167 14.2991 12.85 15.0166L20.9167 21.4833C21.1833 21.7166 21.55 21.8333 21.9167 21.8333C22.6667 21.8333 23.25 21.2333 23.25 20.5V6.66663Z"
                             fill="black"
@@ -189,7 +190,6 @@ const Home = () => {
                       </video>
                       <div onClick={handleNext} className={styles.controllerbutton}>
                         <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          {/* SVG Path for Next button */}
                           <path
                             d="M10.75 21.8333C10.75 22.5667 11.3333 23.1667 12.0833 23.1667C12.45 23.1667 12.8167 23.05 13.0833 22.8167L21.15 16.3509C22.0833 15.6333 22.0833 14.2009 21.15 13.4833L13.0833 7.01663C12.8167 6.78329 12.45 6.66663 12.0833 6.66663C11.3333 6.66663 10.75 7.26663 10.75 7.99996V21.8333Z"
                             fill="black"
@@ -204,9 +204,6 @@ const Home = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.secondAside}>
-          <Aside />
         </div>
       </div>
     </RecoilRoot>
