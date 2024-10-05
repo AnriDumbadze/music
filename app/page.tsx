47bafc8c72ac70b1b11d9885fadee5c829382325
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-10-05 12:17:30 +0400

    aaa
    aaa

--- app/page.tsx
+++ app/page.tsx
@@ -40,16 +40,17 @@ interface Music {
 
 const Home = () => {
   const [query, setQuery] = useState<string>("");
-  const [themeColor, setThemeColor] = useState<string>(getCookie("theme") || "");
+  const [themeColor, setThemeColor] = useState<string>(
+    getCookie("theme") || ""
+  );
   const [artistData, setArtistData] = useState<Artist[]>([]);
   const [musicData, setMusicData] = useState<Music[]>([]);
   const [showPlayer, setShowPlayer] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentTrackTime, setCurrentTrackTime] = useState(0);
-
   useEffect(() => {
     const updateTheme = () => {
-      const newTheme = getCookie("theme") || "light"; 
+      const newTheme = getCookie("theme") || "light";
       setThemeColor(newTheme);
     };
 
@@ -59,7 +60,6 @@ const Home = () => {
 
     return () => clearInterval(themeInterval);
   }, []);
-
   // Fetch artist data
   useEffect(() => {
     const userToken = getCookie("userToken");
@@ -99,13 +99,17 @@ const Home = () => {
   };
 
   const handleBack = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + musicData.length) % musicData.length); // Handle negative index wrap
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + musicData.length) % musicData.length
+    ); // Handle negative index wrap
     setCurrentTrackTime(0);
   };
 
   // Handle selecting a song from the music list without modifying the musicData array
   const handleMusicSelect = (musicId: number) => {
-    const selectedMusicIndex = musicData.findIndex((music) => music.id === musicId);
+    const selectedMusicIndex = musicData.findIndex(
+      (music) => music.id === musicId
+    );
     if (selectedMusicIndex !== -1) {
       setCurrentIndex(selectedMusicIndex); // Set the selected song index without altering the array
     }
@@ -134,15 +138,22 @@ const Home = () => {
         <div className={styles.mainAside}>
           <Aside />
         </div>
-        <div className={`${styles.static} ${themeColor === "dark" ? styles.darkStatic : ""}`}>
+        <div
+          className={`${styles.static} ${
+            themeColor === "dark" ? styles.darkStatic : ""
+          }`}
+        >
           <Header />
           <div className={styles.staticFlex}>
             <div className={styles.wrapperContainer}>
               <MusicWrapper
                 cards={artistData.map((artist) => (
                   <ArtistCard
-                    key={artist.id} // Ensuring each ArtistCard has a unique key
-                    artistImg={artist.image[artist.image.length - 1]?.url || "/Images/artist.png"}
+                    key={artist.id} // Make sure this is present
+                    artistImg={
+                      artist.image[artist.image.length - 1]?.url ||
+                      "/Images/artist.png"
+                    }
                     artistName={artist.firstName}
                     artistType="Artist"
                     biography={artist.biography}
@@ -150,11 +161,15 @@ const Home = () => {
                 ))}
                 name="Popular artists"
               />
+
               <MusicWrapper
                 cards={musicData.map((item) => (
                   <div key={item.id} onClick={() => handleMusicSelect(item.id)}>
                     <MusicCard
-                      url={item.image[item.image.length - 1]?.url || "/Images/popHit.png"}
+                      url={
+                        item.image[item.image.length - 1]?.url ||
+                        "/Images/popHit.png"
+                      }
                       author={item.artist.firstName}
                       songTitle={item.name}
                       id={item.id}
@@ -169,27 +184,57 @@ const Home = () => {
                 {showPlayer && currentSong ? (
                   <>
                     <div className={styles.trackInfo}>
-                      <img src={currentSong.image[0]?.url || "default-album-art.jpg"} alt="Album Art" className={styles.albumArt} />
+                      <img
+                        src={
+                          currentSong.image[0]?.url || "default-album-art.jpg"
+                        }
+                        alt="Album Art"
+                        className={styles.albumArt}
+                      />
                       <div className={styles.trackDetails}>
                         <h3 className={styles.track}>{currentSong.name}</h3>
                         <p>{currentSong.artist.firstName}</p>
                       </div>
                     </div>
                     <div className={styles.videoContainer}>
-                      <div onClick={handleBack} className={styles.controllerbutton}>
-                        <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <div
+                        onClick={handleBack}
+                        className={styles.controllerbutton}
+                      >
+                        <svg
+                          width="33"
+                          height="32"
+                          viewBox="0 0 33 32"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          {/* SVG Path for Back button */}
                           <path
                             d="M23.25 6.66663C23.25 5.93329 22.6667 5.33329 21.9167 5.33329C21.55 5.33329 21.1833 5.44996 20.9167 5.68329L12.85 12.1491C11.9167 12.8666 11.9167 14.2991 12.85 15.0166L20.9167 21.4833C21.1833 21.7166 21.55 21.8333 21.9167 21.8333C22.6667 21.8333 23.25 21.2333 23.25 20.5V6.66663Z"
                             fill="black"
                           />
                         </svg>
                       </div>
-                      <video ref={videoRef} className={styles.lineVideo} controls>
+                      <video
+                        ref={videoRef}
+                        className={styles.lineVideo}
+                        controls
+                      >
                         <source src={currentSong.mp3.url} type="video/mp4" />
                         Your browser does not support the video tag.
                       </video>
-                      <div onClick={handleNext} className={styles.controllerbutton}>
-                        <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <div
+                        onClick={handleNext}
+                        className={styles.controllerbutton}
+                      >
+                        <svg
+                          width="33"
+                          height="32"
+                          viewBox="0 0 33 32"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          {/* SVG Path for Next button */}
                           <path
                             d="M10.75 21.8333C10.75 22.5667 11.3333 23.1667 12.0833 23.1667C12.45 23.1667 12.8167 23.05 13.0833 22.8167L21.15 16.3509C22.0833 15.6333 22.0833 14.2009 21.15 13.4833L13.0833 7.01663C12.8167 6.78329 12.45 6.66663 12.0833 6.66663C11.3333 6.66663 10.75 7.26663 10.75 7.99996V21.8333Z"
                             fill="black"
@@ -204,6 +249,9 @@ const Home = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className={styles.secondAside}>
+          <Aside />
         </div>
       </div>
     </RecoilRoot>
