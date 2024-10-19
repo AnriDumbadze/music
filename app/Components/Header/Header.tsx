7c57f105ca8988f55e7815c0bfda8cec5fd68405
BatoniGyro <gabrielkvergelidze19@gmail.com>
2024-10-19 12:25:08 +0400

    fix

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -5,23 +5,32 @@ import Icon from "../Icon/Icon";
 import styles from "./Header.module.scss";
 interface Props{
   onchange?: (e: React.ChangeEvent<HTMLInputElement>) => void; 
+  isSeachPage?: boolean
 }
 const Header = (props: Props) => {
   const [themeColor, setThemeColor] = useState<string | null>(getCookie("theme") ?? null);
+  const [isSeachPage, setSeatchPage] = useState<any>()
+  useEffect(() => {
+    if(props.isSeachPage) {
+      setSeatchPage(
+        <div className={styles.searchContainer}>
+        <Icon name={"searchIcon"} isActive={false} onClick={() => {}} />
+        <input onChange={props.onchange} className={styles.noBorder} placeholder="search" />
+      </div>
+      )
+    } else {
+      <div></div>
+    }
+  }, [])
+
 
   
   return (
     <div className={`${styles.darkHeader} ${themeColor === 'dark' ? styles.darkHeader : ''}`}>
       <div className={styles.headerContent}>
-        <div className={styles.arrowContainer}>
-          <Icon height={"40px"} width={"40px"} name={"Arrow"} isActive={false} onClick={() => {}} />
-          <Icon height={"40px"} width={"40px"} name={"rightArr"} isActive={false} onClick={() => {}} />
-        </div>
-
-        <div className={styles.searchContainer}>
-          <Icon name={"searchIcon"} isActive={false} onClick={() => {}} />
-          <input onChange={props.onchange} className={styles.noBorder} placeholder="search" />
-        </div>
+        {
+          isSeachPage
+        }
       </div>
     </div>
   );
